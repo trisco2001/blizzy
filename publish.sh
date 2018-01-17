fddb2b6e3e913b808c9467d39e1dad9e386d021c
Tristan Leonard <tristanl@slalom.com>
2018-01-16 22:10:11 -0800

    Simplified publish script.

--- publish.sh
+++ publish.sh
@@ -1,17 +1,9 @@
 rm -f blizzy.zip
-rm -rf build/
-
-echo "Copying from Winders"
-mkdir build
-cp -r  . build/
 
 echo "Zipping new Archive"
-cd build
-zip -X -r ../blizzy.zip *
-cd ..
+zip -X -r blizzy.zip *
 
 echo "Updating Function..."
 aws lambda update-function-code --function-name item-level --zip-file fileb://blizzy.zip
 
 rm -f blizzy.zip
-rm -rf build/
